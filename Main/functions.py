8b23081a7210855c248a22415bb17f9f60142996
SameeranB <sameeranbandishti93@gmail.com>
2019-10-21 17:52:59 +0530

    Debug

--- Main/functions.py
+++ Main/functions.py
@@ -6,12 +6,9 @@ from Main.models import Block
 def coords(points):
 
     # Weighted signal strength
-    print("points" + str(points))
     res = json.loads(points)
-    print(type(res))
     a=list()
     for index, val in enumerate(res):
-      print(index, val)
 
       a.append(tuple(val))
     print("After" + str(a))
