2a45d1ac76c47b7e20e9fc92143ef4b328d9089b
SameeranB <sameeranbandishti93@gmail.com>
2019-10-21 17:40:28 +0530

    Fixed string list error

--- Main/functions.py
+++ Main/functions.py
@@ -1,29 +1,29 @@
+import json
+
 from Main.models import Block
 
 
 def coords(points):
-    """ Given points in (x,y, signal) format, approximate the position (x,y).
 
-        Reading:
-        * http://stackoverflow.com/questions/10329877/how-to-properly-triangulate-gsm-cell-towers-to-get-a-location
-        * http://www.neilson.co.za/?p=364
-        * http://gis.stackexchange.com/questions/40660/trilateration-algorithm-for-n-amount-of-points
-        * http://gis.stackexchange.com/questions/2850/what-algorithm-should-i-use-for-wifi-geolocation
-    """
     # Weighted signal strength
-    print(points)
-    points = points.strip('][').split(', ')
-    for i in range(len(points)):
-        points[i] = tuple(points[i])
-    print("After" + points)
+    # print(points)
+    res = json.loads(points)
+    print(type(res))
+    a=list()
+    for index, val in enumerate(res):
+      print(index, val)
 
-    ws = sum(p[2] for p in points)
-    points = tuple( (x,y,signal/ws) for (x,y,signal) in points )
+      a.append(tuple(val))
+    print("After" + str(a))
+
+    ws = sum(p[2] for p in a)
+    a = tuple( (x,y,signal/ws) for (x,y,signal) in a )
 
     # Approximate
     return (
-        sum(p[0]*p[2] for p in points), # x
-        sum(p[1]*p[2] for p in points) # y
+        sum(p[0]*p[2] for p in a), # x
+        sum(p[1]*p[2] for p in a) # y
     )
+
 
 
